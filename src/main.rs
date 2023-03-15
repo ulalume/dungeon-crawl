1e026865265e36921e6e3028729a86e31e217121
kbt <kubota@softdevice.co.jp>
2023-03-15 19:18:37 +0900

    1マスごとの移動

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_tweening::{lens::*, *};
 use ldtk::Ldtk;
+use std::fmt;
+use std::str::FromStr;
 use std::{f32::consts::PI, time::Duration};
 
 fn main() {
@@ -15,6 +17,46 @@ fn main() {
         .add_system(setup_cat_animation)
         .add_system(update_player)
         .run();
+}
+
+fn get_transform(direction: &Direction, x: f32, z: f32) -> Transform {
+    Transform {
+        translation: Vec3::new(x, 0.0, z),
+        rotation: match direction {
+            Direction::Up => Quat::from_rotation_y(0.0),
+            Direction::Right => Quat::from_rotation_y(-PI * 0.5),
+            Direction::Down => Quat::from_rotation_y(PI),
+            Direction::Left => Quat::from_rotation_y(PI * 0.5),
+        },
+        ..default()
+    }
+}
+fn get_cat_transform(direction: &Direction, x: f32, z: f32) -> Transform {
+    let mut transform = get_transform(direction, x, z);
+    transform.rotate_y(PI);
+    transform
+}
+fn get_wall_transform(direction: &Direction, x: f32, z: f32) -> Transform {
+    let mut transform = get_transform(direction, x, z);
+    transform.translation += match direction {
+        Direction::Up => Vec3::NEG_Z,
+        Direction::Right => Vec3::X,
+        Direction::Down => Vec3::Z,
+        Direction::Left => Vec3::NEG_X,
+    } * 0.5
+        + Vec3::new(0.0, 0.5, 0.0);
+    transform
+}
+fn get_player_transform(direction: &Direction, x: f32, z: f32) -> Transform {
+    let mut transform = get_transform(direction, x, z);
+    transform.translation += match direction {
+        Direction::Up => Vec3::Z,
+        Direction::Right => Vec3::NEG_X,
+        Direction::Down => Vec3::NEG_Z,
+        Direction::Left => Vec3::X,
+    } * 0.4
+        + Vec3::new(0.0, 0.5, 0.0);
+    transform
 }
 
 fn setup(
@@ -63,32 +105,8 @@ fn setup(
     let scene_cat = asset_server.load("cat.glb#Scene0");
     commands.insert_resource(CatAnimation(asset_server.load("cat.glb#Animation0")));
 
-    let get_transform = |direction: &str| match direction.to_lowercase().as_str() {
-        "up" => Transform {
-            translation: Vec3::NEG_Z * 0.5,
-            rotation: Quat::from_rotation_y(0.0),
-            ..default()
-        },
-        "right" => Transform {
-            translation: Vec3::X * 0.5,
-            rotation: Quat::from_rotation_y(-PI * 0.5),
-            ..default()
-        },
-        "down" => Transform {
-            translation: Vec3::Z * 0.5,
-            rotation: Quat::from_rotation_y(PI),
-            ..default()
-        },
-        "left" => Transform {
-            translation: Vec3::NEG_X * 0.5,
-            rotation: Quat::from_rotation_y(PI * 0.5),
-            ..default()
-        },
-        _ => Transform::default(),
-    };
     let spawn_wall = |commands: &mut Commands, direction: &str, x: f32, z: f32| {
-        let mut transform = get_transform(direction);
-        transform.translation += Vec3::new(x, 0.5, z);
+        let transform = get_wall_transform(&direction.parse::<Direction>().unwrap(), x, z);
         commands.spawn(MaterialMeshBundle {
             mesh: mesh_wall.clone(),
             material: material_wall.clone(),
@@ -108,23 +126,28 @@ fn setup(
             ..default()
         });
     };
-    let spawn_cat =
-        |commands: &mut Commands, direction: &str, message: Option<String>, x: f32, z: f32| {
-            let mut transform = get_transform(direction);
-            transform.translation += Vec3::new(x, 0.0, z);
-            transform.rotate_y(PI);
-            commands.spawn((
-                Cat { message },
-                SceneBundle {
-                    scene: scene_cat.clone(),
-                    transform,
-                    ..default()
-                },
-            ));
-        };
+    let spawn_cat = |commands: &mut Commands,
+                     direction: &Direction,
+                     message: Option<String>,
+                     x: f32,
+                     z: f32| {
+        commands.spawn((
+            Cat { message },
+            SceneBundle {
+                scene: scene_cat.clone(),
+                transform: get_cat_transform(direction, x, z),
+                ..default()
+            },
+        ));
+    };
 
     // spawn tiles
     let mut camera_transform = Transform::from_translation(Vec3::new(0.0, 0.5, 0.0));
+    let mut player_position = Position {
+        direction: Direction::Left,
+        x: 0,
+        z: 0,
+    };
     let level = level.unwrap();
     if let Some(layer_instances) = level
         .levels
@@ -148,10 +171,15 @@ fn setup(
                         let z = entity.grid[1] as f32;
                         match entity.identifier.as_str() {
                             "PlayerStart" => {
-                                if let Some(direction) = direction {
-                                    camera_transform.rotation = get_transform(&direction).rotation;
-                                }
-                                camera_transform.translation += Vec3::new(x, 0.0, z);
+                                let direction = direction
+                                    .map(|s| s.as_str())
+                                    .unwrap_or("right")
+                                    .parse::<Direction>()
+                                    .unwrap();
+                                camera_transform = get_player_transform(&direction, x, z);
+                                player_position.x = x as i32;
+                                player_position.z = z as i32;
+                                player_position.direction = direction;
                                 println!("{:?}", camera_transform);
                             }
                             "Cat" => {
@@ -169,7 +197,11 @@ fn setup(
                                     });
                                 spawn_cat(
                                     &mut commands,
-                                    direction.map(|s| s.as_str()).unwrap_or("up"),
+                                    &direction
+                                        .map(|s| s.as_str())
+                                        .unwrap_or("right")
+                                        .parse::<Direction>()
+                                        .unwrap(),
                                     message,
                                     x,
                                     z,
@@ -220,72 +252,151 @@ fn setup(
     // spawn camera
     commands.spawn((
         Player,
+        player_position,
         Camera3dBundle {
             transform: camera_transform,
+            projection: Projection::Perspective(PerspectiveProjection {
+                fov: PI / 2.5,
+                ..default()
+            }),
             ..default()
         },
     ));
 }
 
+enum Direction {
+    Right,
+    Up,
+    Left,
+    Down,
+}
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match *self {
+                Direction::Right => "right",
+                Direction::Up => "up",
+                Direction::Left => "left",
+                Direction::Down => "down",
+            }
+        )
+    }
+}
+impl FromStr for Direction {
+    type Err = ();
+    fn from_str(input: &str) -> Result<Direction, Self::Err> {
+        return match input.to_lowercase().as_str() {
+            "right" => Ok(Direction::Right),
+            "up" => Ok(Direction::Up),
+            "left" => Ok(Direction::Left),
+            "down" => Ok(Direction::Down),
+            _ => Err(()),
+        };
+    }
+}
 #[derive(Component)]
 struct Player;
 
+#[derive(Component)]
+struct Position {
+    direction: Direction,
+    x: i32,
+    z: i32,
+}
+
+impl Position {
+    fn go_forward(&mut self) {
+        match self.direction {
+            Direction::Right => self.x += 1,
+            Direction::Up => self.z -= 1,
+            Direction::Left => self.x -= 1,
+            Direction::Down => self.z += 1,
+        };
+    }
+    fn go_backward(&mut self) {
+        match self.direction {
+            Direction::Right => self.x -= 1,
+            Direction::Up => self.z += 1,
+            Direction::Left => self.x += 1,
+            Direction::Down => self.z -= 1,
+        };
+    }
+    fn rotate_right(&mut self) {
+        self.direction = match self.direction {
+            Direction::Right => Direction::Down,
+            Direction::Up => Direction::Right,
+            Direction::Left => Direction::Up,
+            Direction::Down => Direction::Left,
+        };
+    }
+    fn rotate_left(&mut self) {
+        self.direction = match self.direction {
+            Direction::Right => Direction::Up,
+            Direction::Up => Direction::Left,
+            Direction::Left => Direction::Down,
+            Direction::Down => Direction::Right,
+        };
+    }
+}
+
 fn update_player(
-    mut commands: Commands,
     keys: Res<Input<KeyCode>>,
-    transforms: Query<(Entity, &Transform), With<Player>>,
+    mut commands: Commands,
+    mut query: Query<(Entity, &Transform, &mut Position), With<Player>>,
 ) {
-    if transforms.is_empty() {
+    if query.is_empty() {
         return;
     }
-    let (entity, transform) = transforms.single();
+    let (entity, transform, mut position) = query.single_mut();
+    let mut changed = false;
     if keys.just_pressed(KeyCode::W) {
-        commands
-            .entity(entity)
-            .insert(Animator::<Transform>::new(Tween::new(
-                EaseFunction::QuadraticOut,
-                Duration::from_millis(200),
-                TransformPositionLens {
-                    start: transform.translation,
-                    end: transform.translation + transform.rotation * Vec3::NEG_Z,
-                },
-            )));
+        position.go_forward();
+        changed = true
     }
     if keys.just_pressed(KeyCode::S) {
-        commands
-            .entity(entity)
-            .insert(Animator::<Transform>::new(Tween::new(
-                EaseFunction::QuadraticOut,
-                Duration::from_millis(200),
-                TransformPositionLens {
-                    start: transform.translation,
-                    end: transform.translation + transform.rotation * Vec3::Z,
-                },
-            )));
+        position.go_backward();
+        changed = true
     }
     if keys.just_pressed(KeyCode::A) {
-        commands
-            .entity(entity)
-            .insert(Animator::<Transform>::new(Tween::new(
-                EaseFunction::QuadraticOut,
-                Duration::from_millis(200),
-                TransformRotationLens {
-                    start: transform.rotation,
-                    end: transform.rotation * Quat::from_rotation_y(PI * 0.5),
-                },
-            )));
+        position.rotate_left();
+        changed = true
     }
     if keys.just_pressed(KeyCode::D) {
-        commands
-            .entity(entity)
-            .insert(Animator::<Transform>::new(Tween::new(
-                EaseFunction::QuadraticOut,
-                Duration::from_millis(200),
-                TransformRotationLens {
-                    start: transform.rotation,
-                    end: transform.rotation * Quat::from_rotation_y(-PI * 0.5),
-                },
-            )));
+        position.rotate_right();
+        changed = true
+    }
+
+    if !changed {
+        return;
+    }
+
+    let new_transform =
+        get_player_transform(&position.direction, position.x as f32, position.z as f32);
+
+    commands
+        .entity(entity)
+        .insert(Animator::<Transform>::new(Tween::new(
+            EaseFunction::QuadraticOut,
+            Duration::from_millis(200),
+            MyTransformLens {
+                start: (transform.translation, transform.rotation),
+                end: (new_transform.translation, new_transform.rotation),
+            },
+        )));
+}
+
+struct MyTransformLens {
+    start: (Vec3, Quat),
+    end: (Vec3, Quat),
+}
+impl Lens<Transform> for MyTransformLens {
+    fn lerp(&mut self, target: &mut Transform, ratio: f32) {
+        let (start_vec, start_rot) = self.start;
+        let (end_vec, end_rot) = self.end;
+        target.translation = start_vec + (end_vec - start_vec) * ratio;
+        target.rotation = start_rot.slerp(end_rot, ratio);
     }
 }
 
